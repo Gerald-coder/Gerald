5aa65da33ca578d2f61d81cb9cb09f2101528090
Oduigwe Gerald <paulsongerald00@gmail.com>
2024-01-20 00:21:28 +0100

    started working on the parallax components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss";
 // import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Navr from "./components/Navbar/fNav";
+import Parallax from "./components/parallax/Parallax";
 
 function App() {
   return (
@@ -12,12 +13,17 @@ function App() {
       <section id="Navbar" className="hero__bg">
         <Hero />
       </section>
-      {/* <section>parralax</section>
+      <section className="parallax1__bg">
+        <Parallax type="services" />
+      </section>
       <section>services</section>
+      <section className="parallax2__bg">
+        <Parallax type="portfolio" />
+      </section>
       <section>talk to me</section>
       <section>portfolio</section>
       <section id="Contact">contact</section>
-      <section>footer</section> */}
+      <section>footer</section>
     </div>
   );
 }
---
+++ src/components/parallax/Parallax.jsx
@@ -0,0 +1,15 @@
+import "./parallax.scss";
+
+/* eslint-disable */
+
+function Parallax({ type }) {
+  return (
+    <div className="parallax section__margin section__padding">
+      <h1>{type === "services" ? "What I do ?" : "What I did ?"}</h1>
+      <div className="mountains"></div>
+      <div className="stars"></div>
+    </div>
+  );
+}
+
+export default Parallax;
