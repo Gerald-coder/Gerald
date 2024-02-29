dd94af3b9faaf2e3d5158a0206a3e808b8ea74e9
Oduigwe Gerald <paulsongerald00@gmail.com>
2024-03-01 00:54:38 +0100

    added some animation

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,27 +1,41 @@
 import "./contact.scss";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function Contact() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  });
   return (
     <div className="contact section__margin section__padding">
-      <div className="textContainer">
-        <h1 className="title"> let&apos;s work together</h1>
-        <div className="item">
+      <div
+        className="textContainer"
+        // data-aos="zoom-in"
+        // data-aos="fade-right"
+        // data-aos="fade-left"
+        data-aos="fade-up"
+      >
+        <h1 className="title" data-aos="fade-up">
+          let&apos;s work together
+        </h1>
+        <div className="item" data-aos="fade-up">
           <h2>mail</h2>
           <span>paulsongerald00@gmail.com</span>
         </div>
-        <div className="item">
+        <div className="item" data-aos="fade-up">
           <h2>phone</h2>
           <span>+234 704 444 16</span>
           <br />
           <span>+234 810 397 80</span>
         </div>
       </div>
-      <div className="formContainer">
+      <div className="formContainer" data-aos="fade-up">
         <form action="">
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <textarea rows={8} cols={80} placeholder="message"></textarea>
-          <button>Submit</button>
+          <button data-aos="fade-left">Submit</button>
         </form>
       </div>
     </div>
